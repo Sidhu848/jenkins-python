6b31c821e4a3ffb3423357e4902c301cd0aa625d
Sidhu848 <sudarshan.sudeer@gmail.com>
2025-10-09 17:22:12 +0530

    changes regarding the app port

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def get_weather(city):
         return jsonify({"error": "Weather data not available"}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=5000)
